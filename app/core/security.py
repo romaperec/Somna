4ed37605ccd00caefdd4f122f2c3ad60b39502ec
romaperec <cpser621@gmail.com>
2026-05-29 20:54:20 +0300

    feat(core): implement PasswordSecurityService with argon2

---
+++ app/core/security.py
@@ -0,0 +1,12 @@
+from argon2 import PasswordHasher
+
+
+class PasswordSecurityService:
+    def __init__(self):
+        self._ph = PasswordHasher()
+
+    def hash(self, password: str) -> str:
+        return self._ph.hash(password)
+
+    def verify(self, password: str, hashed_password: str) -> bool:
+        return self._ph.verify(password, hashed_password)

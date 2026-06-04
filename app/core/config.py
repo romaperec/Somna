e9477a346b4d5ec3ba5fffff180d7f30c295ecf0
romaperec <cpser621@gmail.com>
2026-06-04 20:25:48 +0300

    feat(config): add settings for jwt

--- app/core/config.py
+++ app/core/config.py
@@ -36,6 +36,13 @@ class DatabaseSettings(BaseModel):
     }
 
 
+class JWTSettings(BaseModel):
+    algorithm: str = "HS256"
+    secret_key: str
+    access_token_expire_minutes: int = 30
+    refresh_token_expire_days: int = 7
+
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=(".env.template", ".env"),

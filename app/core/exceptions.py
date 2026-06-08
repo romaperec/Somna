53fe5dfab39e84d34b03cf41672bf2be2a08e93c
romaperec <cpser621@gmail.com>
2026-06-08 14:56:47 +0300

    feat(exceptions): add status_code attribute to base exception

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -1,7 +1,10 @@
 class AppBaseException(Exception):
     message: str = "An unexpected exception occurred."
+    status_code: int = 500
 
-    def __init__(self, message: str) -> None:
-        if message:
-            self.message = message
-        super().__init__(self.message)
+    def __init__(self, message: str | None = None, status_code: int | None = None):
+        final_message: str = message or self.message or ""
+        super().__init__(final_message)
+        self.message = final_message
+        if status_code:
+            self.status_code = status_code

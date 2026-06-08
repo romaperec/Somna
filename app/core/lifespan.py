e24f42fab3f6d65b6905d906e23af691a47de508
romaperec <cpser621@gmail.com>
2026-06-08 15:03:11 +0300

    feat(lifespan): add redis_client to app.state

--- app/core/lifespan.py
+++ app/core/lifespan.py
@@ -1,42 +1,43 @@
 from contextlib import asynccontextmanager
 
-import redis.asyncio as aioredis
 from fastapi import FastAPI
 from loguru import logger
+from redis.asyncio import BlockingConnectionPool, Redis
 
 from app.core.config import settings
 from app.core.logging import setup_logging
 
-redis_pool: aioredis.BlockingConnectionPool | None = None
-
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    global redis_pool
     setup_logging()
     logger.info("Initializing application components")
 
     logger.info("Connecting to Redis")
-    redis_pool = aioredis.BlockingConnectionPool(
+    redis_pool = BlockingConnectionPool(
         host=settings.redis.host,
         port=settings.redis.port,
         max_connections=settings.redis.max_connections,
         timeout=settings.redis.timeout,
         decode_responses=True,
     )
+    redis_client = Redis(connection_pool=redis_pool, db=settings.redis.db_cache)
 
     try:
-        async with aioredis.Redis(connection_pool=redis_pool) as client:
-            await client.ping()
-        logger.info("Redis connected successfully")
+        await redis_client.ping()
+        logger.info(f"Redis connected successfully. DB {settings.redis.db_cache}")
     except Exception as e:
-        logger.error(f"Failed to connect to Redis: {e}")
+        logger.critical(f"Failed to connect to Redis: {e}")
+        await redis_pool.disconnect()
         raise e
+
+    app.state.redis = redis_client
 
     yield
 
     logger.info("Shutting down application")
     if redis_pool:
-        logger.info("Closing redis connection pool")
+        logger.info("Closing redis client and connection pool")
+        await redis_client.aclose()
         await redis_pool.disconnect()
-        logger.info("Redis connection pool closed")
+        logger.info("Redis connections closed successfully")

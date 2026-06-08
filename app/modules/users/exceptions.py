dcb1471132ea9802d7da5c22a72b391f5fb54862
romaperec <cpser621@gmail.com>
2026-06-08 15:07:22 +0300

    feat(users): add status_code to all user exceptions

--- app/modules/users/exceptions.py
+++ app/modules/users/exceptions.py
@@ -7,19 +7,24 @@ class UserBaseException(AppBaseException):
 
 class UserEmailAlreadyExistsException(UserBaseException):
     message = "User with this email already exists."
+    status_code = 409
 
 
 class UserUsernameExistsException(UserBaseException):
     message = "User with this username already exists."
+    status_code = 409
 
 
 class UserNotFoundException(UserBaseException):
     message = "User not found."
+    status_code = 404
 
 
 class UserInvalidPasswordException(UserBaseException):
     message = "Password is invalid."
+    status_code = 401
 
 
 class UserPasswordsMatchException(UserBaseException):
     message = "The new password must not match the current one."
+    status_code = 400

d9547c2531ec50edf28b5af16e3ab7ed30e7d237
romaperec <cpser621@gmail.com>
2026-06-10 20:16:16 +0300

    feat(auth): add user login endpoint

--- app/modules/auth/router.py
+++ app/modules/auth/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, Response
 
 from app.core.config import settings
 from app.modules.auth.dependencies import get_auth_service
-from app.modules.auth.schemas import TokenPair
+from app.modules.auth.schemas import TokenPair, UserLogin
 from app.modules.auth.service import AuthService
 from app.modules.users.schemas import UserCreate
 
@@ -17,6 +17,21 @@ def status():
 @router.post("/register", response_model=TokenPair, status_code=201)
 async def register(user: UserCreate, response: Response, service: AuthService = Depends(get_auth_service)):
     tokens: TokenPair = await service.register_user(user)
+
+    response.set_cookie(
+        key="refresh_token",
+        value=tokens.refresh_token,
+        httponly=True,
+        secure=False,
+        samesite="lax",
+        max_age=settings.jwt.refresh_token_expire_days * 24 * 60 * 60,
+    )
+
+    return tokens
+
+@router.post("/login", response_model=TokenPair)
+async def login(user: UserLogin, response: Response, service: AuthService = Depends(get_auth_service)):
+    tokens: TokenPair = await service.login_user(user)
 
     response.set_cookie(
         key="refresh_token",

7b1dfcf19578465170114bf9aa52f5a32f81e884
romaperec <cpser621@gmail.com>
2026-06-10 17:30:50 +0300

    refactor(users): remove () for uuid generator in user model

--- app/modules/users/models.py
+++ app/modules/users/models.py
@@ -7,7 +7,7 @@ from app.core.base import Base
 
 
 class User(Base):
-    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4())
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
     username: Mapped[str] = mapped_column(String(32), unique=True)
     description: Mapped[str] = mapped_column(String(500))
     email: Mapped[str] = mapped_column(String(254), unique=True)

9cec0b8b44cd2fb16c99c3410d08a2dcc449c5ab
romaperec <cpser621@gmail.com>
2026-06-10 16:59:17 +0300

    feat(main): add exception handler for AppBaseException

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
+from app.core.exceptions import AppBaseException
 from app.core.lifespan import lifespan
 
 app = FastAPI(lifespan=lifespan)
+
+@app.exception_handler(AppBaseException)
+async def app_base_exception_handler(request: Request, exc: AppBaseException):
+    return JSONResponse(status_code=exc.status_code, content={"status": "error", "message": exc.message})

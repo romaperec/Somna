0c8a9f961202e78e363b58923811dfaa9b6f4747
romaperec <cpser621@gmail.com>
2026-06-26 20:21:42 +0300

    feat(main): register users router in app

--- app/main.py
+++ app/main.py
@@ -4,9 +4,11 @@ from fastapi.responses import JSONResponse
 from app.core.exceptions import AppBaseException
 from app.core.lifespan import lifespan
 from app.modules.auth.router import router as auth_router
+from app.modules.users.router import router as user_router
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(auth_router)
+app.include_router(user_router)
 
 @app.exception_handler(AppBaseException)
 async def app_base_exception_handler(request: Request, exc: AppBaseException):

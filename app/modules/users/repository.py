07bb6f73b25484278d75014ccad6018243c0bbd8
romaperec <cpser621@gmail.com>
2026-06-26 15:52:42 +0300

    refactor(users): implement get_by_id caching and add get_orm_by_id method

--- app/modules/users/repository.py
+++ app/modules/users/repository.py
@@ -1,19 +1,37 @@
 import uuid
 
+from redis.asyncio import Redis
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
 from app.modules.users.models import User
+from app.modules.users.schemas import UserPrivateResponse
 
 
 class UserRepository:
-    def __init__(self, session: AsyncSession):
+    def __init__(self, redis: Redis,  session: AsyncSession):
+        self.redis = redis
         self.session = session
 
-    async def get_by_id(self, user_id: uuid.UUID) -> User | None:
+    async def get_orm_by_id(self, user_id: uuid.UUID) -> User | None:
         user = await self.session.execute(select(User).where(User.id == user_id))
         user = user.scalar_one_or_none()
         return user
+
+    async def get_by_id(self, user_id: uuid.UUID, ttl: int = 900) -> UserPrivateResponse | None:
+        cached_user = await self.redis.get(f"user:{user_id}")
+        if cached_user:
+            return UserPrivateResponse.model_validate_json(cached_user)
+        user = await self.session.execute(select(User).where(User.id == user_id))
+        user = user.scalar_one_or_none()
+
+        if not user:
+            return None
+
+        user_schema = UserPrivateResponse.model_validate(user)
+        await self.redis.set(f"user:{user_id}", user_schema.model_dump_json(), ex=ttl)
+
+        return user_schema
 
     async def get_by_email(self, email: str) -> User | None:
         user = await self.session.execute(select(User).where(User.email == email))

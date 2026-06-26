4037bd5d93f59dd33fa8eba668262f05d5d1b00e
romaperec <cpser621@gmail.com>
2026-06-26 15:56:02 +0300

    feat(users): add get me endpoint

--- app/modules/users/router.py
+++ app/modules/users/router.py
@@ -6,3 +6,7 @@ router = APIRouter(prefix="/users")
 @router.get("/status")
 def status():
     return {"status": "ok"}
+
+@router.get("/me", response_model=UserResponse)
+async def get_user_profile(current_user_id: UUID = Depends(get_current_user_id), user_service: UserService = Depends(get_user_service)):
+    return await user_service.get_profile(current_user_id)

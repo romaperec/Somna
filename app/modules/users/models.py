3c06a7294f63eec9c48d89675235f5f7bb5fa078
romaperec <cpser621@gmail.com>
2026-08-12 18:33:38 +0300

    feat(users): add nullable parameter for User model

--- app/modules/users/models.py
+++ app/modules/users/models.py
@@ -8,7 +8,7 @@ from app.core.base import Base
 
 class User(Base):
     id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
-    username: Mapped[str] = mapped_column(String(32), unique=True)
-    description: Mapped[str] = mapped_column(String(500))
-    email: Mapped[str] = mapped_column(String(254), unique=True)
-    hashed_password: Mapped[str]
+    username: Mapped[str] = mapped_column(String(32))
+    description: Mapped[str] = mapped_column(String(450), nullable=True)
+    email: Mapped[str] = mapped_column(String(254), unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(nullable=True)

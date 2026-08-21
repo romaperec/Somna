1d122aa3c5f5f3bc9bdf8995690760d600623458
romaperec <cpser621@gmail.com>
2026-08-21 17:28:34 +0300

    refactor(users): remove UserUsernameExists exception

--- app/modules/users/exceptions.py
+++ app/modules/users/exceptions.py
@@ -9,21 +9,13 @@ class UserEmailAlreadyExistsException(UserBaseException):
     message = "User with this email already exists."
     status_code = 409
 
-
-class UserUsernameExistsException(UserBaseException):
-    message = "User with this username already exists."
-    status_code = 409
-
-
 class UserNotFoundException(UserBaseException):
     message = "User not found."
     status_code = 404
 
-
 class UserInvalidPasswordException(UserBaseException):
     message = "Password is invalid."
     status_code = 401
-
 
 class UserPasswordsMatchException(UserBaseException):
     message = "The new password must not match the current one."

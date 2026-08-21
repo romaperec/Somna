099ac70b5c8f0205cb2c4fdb58884bb2a3f624ca
romaperec <cpser621@gmail.com>
2026-08-21 16:23:14 +0300

    feat(main): add CORS config

--- app/main.py
+++ app/main.py
@@ -7,6 +7,16 @@ from app.modules.auth.router import router as auth_router
 from app.modules.users.router import router as user_router
 
 app = FastAPI(lifespan=lifespan, version="1.0.0")
+
+# noinspection bad-argument-type
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[settings.app.frontend_host],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth_router)
 app.include_router(user_router)
 

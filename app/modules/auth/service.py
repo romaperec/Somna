c01eab2d99714814a39955ecb868e489580458dc
romaperec <cpser621@gmail.com>
2026-09-04 15:04:32 +0300

    feat(auth): add recovery_password method in service

--- app/modules/auth/service.py
+++ app/modules/auth/service.py
@@ -1,6 +1,8 @@
+from datetime import datetime, timezone, timedelta
 from uuid import UUID
 
 from app.core.jwt_helper import JWTHelper
+from app.core.recovery_helper import RecoveryTokenHelper
 from app.core.security import PasswordSecurityService
 from app.modules.auth.exceptions import (
     AuthenticationFailedException,
@@ -8,17 +10,49 @@ from app.modules.auth.exceptions import (
     MissingTokenException,
 )
 from app.modules.auth.repository import AuthRepository
-from app.modules.auth.schemas import TokenPair, UserLogin
+from app.modules.auth.schemas import TokenPair, UserLogin, RecoveryPassword, ResetPassword
+from app.modules.auth.tasks import task_send_recovery_email
 from app.modules.users.schemas import UserBase, UserCreate
 from app.modules.users.service import UserService
 
 
 class AuthService:
-    def __init__(self, repo: AuthRepository, jwt_service: JWTHelper, user_service: UserService, password_service: PasswordSecurityService):
+    def __init__(self, repo: AuthRepository, jwt_service: JWTHelper, user_service: UserService, password_service: PasswordSecurityService, recovery_helper: RecoveryTokenHelper):
         self.repo = repo
         self.jwt_service = jwt_service
         self.user_service = user_service
         self.password_service = password_service
+        self.recovery_helper = recovery_helper
+
+    async def recovery_password(self, data: RecoveryPassword) -> dict[str, str]:
+        user = await self.user_service.get_for_authentication(data.email)
+        if not user:
+            raise AuthenticationFailedException
+
+        tokens = self.recovery_helper.generate_pair()
+        expires_at = (datetime.now(timezone.utc) + timedelta(minutes=15)).replace(tzinfo=None)
+
+        await self.repo.save_recovery_token(user.id, tokens[1], expires_at)
+
+        await task_send_recovery_email.kiq(email=user.email, token=tokens[0])
+        return {"detail": "Email sent successfully"}
+
+    async def reset_password(self, data: ResetPassword) -> dict[str, str]:
+        token_hash = self.recovery_helper.hash_token(data.token)
+        token = await self.repo.get_recovery_token_by_hash(token_hash)
+        if token is None:
+            raise InvalidTokenException
+
+        if token.is_used == True or token.expires_at < datetime.now(timezone.utc).replace(tzinfo=None):
+            raise InvalidTokenException
+
+        hashed_password = self.password_service.hash(data.password)
+
+        await self.repo.save_recovery_token_status(token.token_hash, True)
+        await self.user_service.save_new_password(token.user_id, hashed_password)
+
+        return {"detail": "Password reset successfully"}
+
 
     async def register_user(self, schema: UserCreate) -> TokenPair:
         user = await self.user_service.register(schema)

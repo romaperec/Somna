29fdf4c7d1992324552ce0d86dd9de0b62a9ecfc
romaperec <cpser621@gmail.com>
2026-09-16 22:23:57 +0300

    refactor(broker): change somna to app.name

--- app/core/tasks.py
+++
@@ -1,5 +0,0 @@
-from taskiq_nats import PullBasedJetStreamBroker
-
-from app.core.config import settings
-
-broker = PullBasedJetStreamBroker(f"nats://{settings.nats.host}:{settings.nats.port}", queue="somna_tasks")
